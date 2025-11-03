86a1075d8d58456625c1fdfb24aa8410d9d96da2
renicotine <renicotine@gmail.com>
2025-11-03 21:30:09 +0300

    add menu

---
+++ scripts/Menu.js
@@ -0,0 +1,20 @@
+async function renderMenu() {
+  const dropdownToggle = document.querySelector(".dropdown");
+
+  const dropdownContent = document.createElement("div");
+  dropdownContent.classList.add("dropdown-content");
+
+  dropdownContent.innerHTML = `
+    <a href="/index.html">К выбору класса</a>
+    <a href="Menu.html">На главную</a>  
+    <a href="Theory.html">Теория</a>
+    <a href="Conditions.html">Условия</a>
+    <a href="Tasks.html">Тренажер</a>
+    <a href="Variants.html">Варианты</a>
+    <a href="VarByAi.html">Варианты от ИИ</a>
+  `;
+
+  dropdownToggle.appendChild(dropdownContent);
+}
+
+document.addEventListener("DOMContentLoaded", renderMenu);
